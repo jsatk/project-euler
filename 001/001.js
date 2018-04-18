f23308f8fa7a0a574ffbb178b7f52ef9d8ac7c51
Jesse Atkinson <jesse@jsatk.us>
2018-04-17 21:27:32 -0700

    Move method in JS solution for 001

--- 001/001.js
+++ 001/001.js
@@ -10,13 +10,13 @@
 // Utility
 const getNumbersBelowN = n => Array.from(Array(n).keys()) // This excludes `n`
 const sumOfArr = arr => arr.reduce((sum, n) => sum + n, 0)
+const getNaturalNumbersBelowN = n => getNumbersBelowN(n).slice(1) // Drops the 0
 
 // Predicates
 const isMultipleOf = n => num => num % n === 0
 const isMultipleOf3Or5 = num => isMultipleOf(3)(num) || isMultipleOf(5)(num)
 
 // Problem-specific functions
-const getNaturalNumbersBelowN = n => getNumbersBelowN(n).slice(1) // Drops the 0
 const naturalNumbersBelow1000 = getNaturalNumbersBelowN(1000)
 const multiplesOf3Or5 = naturalNumbersBelow1000.filter(isMultipleOf3Or5)
 const sumOfMultiplesOf3Or5 = sumOfArr(multiplesOf3Or5)
