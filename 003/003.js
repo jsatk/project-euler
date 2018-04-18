dcc042d2cf500d5441d4bf21f19bac44db7baed9
Jesse Atkinson <jesse@jsatk.us>
2018-04-17 23:14:59 -0700

    Use non-recursive solution for JS for problem 3

--- 003/003.js
+++ 003/003.js
@@ -9,43 +9,42 @@
 // NOTE: The below code works in all modern versions of node as it does not
 // require tail call optimization
 // More traditional solution not using recursion
-// const getAllFactorsFor = n => {
-//   const factors = []
+const getAllFactorsFor = n => {
+  const factors = []
 
-//   // There might be a way to do this that's not O(n^2) but ¯\_(ツ)_/¯
-//   // I also hate `for` and `while` loops, but I'll live since it's encapsulated
-//   // in a method here and doesn't leak.
-//   for (let i = 2; i <= n; i++) {
-//     while (n % i === 0) {
-//       factors.push(i)
-//       n /= i // Shorthand for remainder = remainder / i
-//     }
-//     console.log(i)
-//   }
+  // There might be a way to do this that's not O(n^2) but ¯\_(ツ)_/¯
+  // I also hate `for` and `while` loops, but I'll live since it's encapsulated
+  // in a method here and doesn't leak.
+  for (let i = 2; i <= n; i++) {
+    while (n % i === 0) {
+      factors.push(i)
+      n /= i // Shorthand for remainder = remainder / i
+    }
+  }
 
-//   return factors
-// }
+  return factors
+}
 
 // NOTE: The below recursive method only works as of this writing (2017-11-12)
 // by adding the `--harmony-tailcalls` flag when you run `node` as well as
 // adding `use strict` to the top of the file or using the `--use_strict`
 // flag with node.  I.E. `node --harmony-tailcalls 003.js`
 // Pure functional approach using recursion
-const getAllFactorsFor = (n, i = 2, factors = []) => {
-  if (i > n) return factors
-  const factorsForI = getFactorsForI(i)(n, factors)
-  return getAllFactorsFor(factorsForI.n, i + 1, factorsForI.factors)
-}
+// const getAllFactorsFor = (n, i = 2, factors = []) => {
+//   if (i > n) return factors
+//   const factorsForI = getFactorsForI(i)(n, factors)
+//   return getAllFactorsFor(factorsForI.n, i + 1, factorsForI.factors)
+// }
 
-const getFactorsForI = i => {
-  const getFactorsForN = (n, factors) => {
-    if (n % i !== 0) return {factors, n}
-    const remainder = n /= i // Shorthand for remainder = remainder / i
-    return getFactorsForN(remainder, factors.concat([i]))
-  }
+// const getFactorsForI = i => {
+//   const getFactorsForN = (n, factors) => {
+//     if (n % i !== 0) return {factors, n}
+//     const remainder = n /= i // Shorthand for remainder = remainder / i
+//     return getFactorsForN(remainder, factors.concat([i]))
+//   }
 
-  return getFactorsForN
-}
+//   return getFactorsForN
+// }
 
 const factors = getAllFactorsFor(600851475143)
 const largetsPrimeFactor = factors.sort((a, b) => b - a)[0]
